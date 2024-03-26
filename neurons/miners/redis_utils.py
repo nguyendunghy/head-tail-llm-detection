4e1dc4af4a209f739142a4fd7f5d757e620795fd
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-26 17:52:56 +0700

    verify

--- neurons/miners/redis_utils.py
+++ neurons/miners/redis_utils.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
     # file_path = "/root/c4_dataset/c4/extracted_file/c4-train.00001-of-01024.json"
     # file_path = "/root/c4_dataset/c4/extracted_file/head-1000-00001.json"
     file_path = "/root/c4_dataset/c4/extracted_file/tail-2000-00001.json"
-    load(file_path)
+    # load(file_path)
 
-    # verify_data(file_path)
+    verify_data(file_path)
     bt.logging.info(f"time loading {int(time.time_ns() - start_time)}nanosecond")
