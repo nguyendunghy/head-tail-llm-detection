8b37342d9ec443b35a385f3f3de2cd932d6dadb1
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-31 07:08:33 +0700

    add gpt_zero_api.py

---
+++ neurons/miners/gpt_zero_api.py
@@ -0,0 +1,28 @@
+import requests
+
+URL = 'https://api.gptzero.me/v2/predict/text'
+
+
+def post(document):
+    body = {
+        "document": ""
+    }
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'x-api-key': '61b48856c4af45e8b36723b4135254b5'
+    }
+
+    response = requests.post(URL, json=body, headers=headers)
+
+    # Checking if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print('Failed to post data:', response.status_code)
+
+
+if __name__ == '__main__':
+    post('World War II: During WWII, the demand for crude oil increased dramatically. This boosted the local oil production in Texas, including areas such as Midland County where Plateau is situated. The town experienced economic growth due to increased oil extraction and related activities. 3. Civil Rights Movement (1950s-1960s): While Texas was at the forefront of segregation and racial inequality during this time, small towns like Plateau were also affected. African Americans and Latino residents struggled for rights, and local events might have reflected these national movements. 4.')
