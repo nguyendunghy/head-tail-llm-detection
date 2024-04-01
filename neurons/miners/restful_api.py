48ae160b02d738f7554965aff29e3e734d965937
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-01 22:43:07 +0700

    insert data using api

--- neurons/miners/restful_api.py
+++ neurons/miners/restful_api.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import monitor_data_mysql
 
 app = Flask(__name__)
 
@@ -6,6 +7,25 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.is_json:
+        data = request.get_json()
+        id = data['id']
+        text_hash = data['text_hash']
+        model_type = data['model_type']
+        count_ai = data['count_ai']
+        count_human = data['count_human']
+        input_data = (id, text_hash, model_type, count_ai, count_human)
+        print(input_data)
+        db_connection = monitor_data_mysql.get_db_connection('localhost', '3306')
+        monitor_data_mysql.insert(db_connection, input_data)
+
+        return jsonify({"insert success": True, "data": data}), 200
+    else:
+        return jsonify({"error": "Request must be JSON"}), 400
 
 
 if __name__ == '__main__':
