a5fcc50a25d8d6a4647c2d3d69daf8da04815cc4
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-02 10:01:53 +0700

    add winston ai api

--- neurons/miners/winston_ai_api.py
+++ neurons/miners/winston_ai_api.py
@@ -68,8 +68,11 @@ if __name__ == "__main__":
     doc2 = "Running this command will stop the UFW firewall and deactivate the rules it has enforced, but it won't remove those rules. If you decide to enable UFW again later, your previously set rules will still be there and will become active again.If you prefer netstat or are working on a system where it's already installed, you can achieve similar results. First, if netstat is not installed, you can install the net-tools package that includes it"
     doc3 = "The Flask development server will start, and you can test the /data endpoint by sending a POST request with JSON data in the body using tools like curl, Postman, or any HTTP client library in your preferred programming language.The ss command is included with iproute2, a collection of utilities for controlling networking in Linux. It's available by default on Ubuntu and can be used to display more detailed network statistics."
     doc4 = "Creating a RESTful API to receive data in the request body can be straightforward with the Flask web framework. Flask is a lightweight WSGI web application framework in Python, well-suited for creating RESTful APIs. Below is a simple example that demonstrates how to set up a Flask application with an endpoint that receives JSON data in the request body."
+    doc5 = "In terms of the rate of increase, Rice University (ranked 17th) has the highest tuition hike compared to last year, from $57,200 to $62,800, an increase of 9.9%. This is followed by Stanford University (ranked 3rd) and John Hopkins University (ranked 9th), both of which increased by about 5%, to $62,900 and $65,900 a year, respectively."
+
     start_time = time.time_ns()
-    re = is_ai_generated_list([doc1, doc2, doc3, doc4])
+    input_data = [doc1, doc2, doc3, doc4, doc5] * 10
+    re = is_ai_generated_list(input_data)
     end_time = time.time_ns()
     print("time processing millisecond: " + str((end_time - start_time)/1_000_000))
     print("result:" + str(re))
