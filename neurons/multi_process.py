e330c8b9e11761e8dc03387587675b0b58401c8b
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-05 23:08:27 +0700

    add multi process

--- neurons/multi_process.py
+++ neurons/multi_process.py
@@ -2,6 +2,7 @@
 # example of a program that does not use all cpu cores
 import math
 import time
+from concurrent.futures import ProcessPoolExecutor
 
 
 # define a cpu-intensive task
@@ -10,7 +11,7 @@ def task(arg):
 
 
 # protect the entry point
-if __name__ == '__main__':
+def one_cpu():
     start = time.time_ns()
     # report a message
     print('Starting task...')
@@ -20,4 +21,22 @@ if __name__ == '__main__':
     print(results)
     print('Done.')
     end = time.time_ns()
-    print("time processing: {}".format(str((end - start)//1000_000)))
+    print("time processing: {}".format(str((end - start) // 1000_000)))
+
+
+def multi_cpu():
+    # report a message
+    print('Starting task...')
+    # create the process pool
+    with ProcessPoolExecutor(8) as exe:
+        # perform calculations
+        results = exe.map(task, range(1, 50000))
+    # report a message
+    print('Done.')
+
+
+if __name__ == '__main__':
+    start = time.time_ns()
+    multi_cpu()
+    end = time.time_ns()
+    print('time processing {}'.format(str((end-start)/1000_000)))
