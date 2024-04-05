e0d26ffc1aabf183c70afc43fc97dfeff5755fb1
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-05 18:52:37 +0700

    add class filter human text

--- neurons/miners/gpt_zero.py
+++ neurons/miners/gpt_zero.py
@@ -90,46 +90,11 @@ class PPLModel:
         ppl = int(torch.exp(torch.stack(nlls).sum() / end_loc))
         return ppl
 
-    def find_incorrect_human_text(self, file_path, start_line, end_line):
-        """
-        human writen text has model(text) < 0.5. In this function, we will find al sub-sentence that has mode(text)>0.5
-        even it's human written text
-        :param file_path:
-        :param start_line:
-        :param end_line:
-        :return:
-        """
-        data_aug = DataAugmentator()
-        with open(file_path, 'r') as file:
-            count = 1
-            for line in file:
-                if start_line <= count < end_line:
-                    data = json.loads(line)
-                    list_sub_sentence = data_aug.get_all_sub_sentences(data['text'])
-                    bt.logging.info("list sub sentences: " + str(list_sub_sentence))
-                    bt.logging.info(
-                        "text in line {} has {} sub-sentences".format(str(count), str(len(list_sub_sentence))))
-                    for sub_sentence in list_sub_sentence:
-                        try:
-                            res = self.__call__(sub_sentence)
-                            if res > 0.5:
-                                bt.logging.info(str(count) + ":" + sub_sentence)
-                        except Exception as e:
-                            bt.logging.error(e)
-                count += 1
 
 
 if __name__ == '__main__':
-    # model = PPLModel(device='cpu')
-    # model.load_pretrained('neurons/miners/ppl_model.pk')
-    # text = 'Hello world, i am here'
-    # res = model(text)
-    # print(res)
-    start_time = time.time_ns()
-    # file_path = "/root/c4_dataset/c4/extracted_file/c4-train.00001-of-01024.json"
-    file_path = "/root/c4_dataset/c4/extracted_file/head-1000-00001.json"
-    # file_path = "/root/c4_dataset/c4/extracted_file/head-10000-00001.json"
     model = PPLModel(device='cpu')
     model.load_pretrained('neurons/miners/ppl_model.pk')
-    model.find_incorrect_human_text(file_path, 0, 10001)
-    bt.logging.info(f"time loading {int(time.time_ns() - start_time)}nanosecond")
+    text = 'Hello world, i am here'
+    res = model(text)
+    print(res)
