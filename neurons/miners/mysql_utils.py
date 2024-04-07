46d3b5d430b43279bccd21231f89386aede88cba
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-07 13:10:08 +0700

    test insert from file

--- neurons/miners/mysql_utils.py
+++ neurons/miners/mysql_utils.py
@@ -78,7 +78,7 @@ def insert(db_connection, db, hash_value):
 
 def insert_batch(db_connection, db, list_value):
     bt.logging.info("record list_value: {}, db: {}".format(str(len(list_value)), str(db)))
-
+    bt.logging.info("list_value: " + str(list_value))
     cursor = db_connection.cursor()
 
     sql = "INSERT INTO table_{} (hash) VALUES (%s)".format(str(db))
