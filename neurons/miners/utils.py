b5c959f8ac875b6262fcbb53f273c256136f3356
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-09 11:57:38 +0700

    update code to convert

--- neurons/miners/utils.py
+++ neurons/miners/utils.py
@@ -1,6 +1,7 @@
 import sys
 import bittensor as bt
 
+
 def hash_code(string) -> int:
     h = 0
     if len(string) > 0:
@@ -16,7 +17,7 @@ def create_one_column_file(source_path, dest_path):
         with open(dest_path, 'w') as dest_file:
             for line in source_file:
                 line = line.strip()
-                if len(line) > 0:
+                if len(line) > 0 and len(line.replace(',', '')) > 0:
                     data = line.split(',')
                     if is_first_not_empty_line:
                         converted_data = '\n'.join(data)
