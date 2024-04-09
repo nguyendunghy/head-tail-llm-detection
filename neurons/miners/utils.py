ebba319f403a2ed359244b6cc5cc9b6ae12f701e
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-09 11:45:58 +0700

    add create_one_column_file

--- neurons/miners/utils.py
+++ neurons/miners/utils.py
@@ -1,5 +1,5 @@
 import sys
-
+import bittensor as bt
 
 def hash_code(string) -> int:
     h = 0
@@ -10,20 +10,24 @@ def hash_code(string) -> int:
 
 
 def create_one_column_file(source_path, dest_path):
-    count = 0
+    count = 1
+    is_first_not_empty_line = True
     with open(source_path, 'r') as source_file:
         with open(dest_path, 'w') as dest_file:
             for line in source_file:
-                data = line.split(',')
-                if count == 0:
-                    converted_data = '\n'.join(data)
-                else:
-                    converted_data = '\n' + '\n'.join(data)
-                dest_file.write(converted_data)
+                data = line.strip().split(',')
+                if len(data) > 0:
+                    if is_first_not_empty_line:
+                        converted_data = '\n'.join(data)
+                        is_first_not_empty_line = False
+                    else:
+                        converted_data = '\n' + '\n'.join(data)
+                    dest_file.write(converted_data)
+                bt.logging.info("data at line {} converted".format(str(count)))
                 count += 1
 
 
 if __name__ == '__main__':
     arg1 = sys.argv[1]
     arg2 = sys.argv[2]
-    create_one_column_file(source_path=arg1,dest_path=arg2)
+    create_one_column_file(source_path=arg1, dest_path=arg2)
