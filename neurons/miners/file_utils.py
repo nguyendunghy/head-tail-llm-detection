aa219b62b8f37cdbba5e1b6361c4da9bc03280e0
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-09 07:20:59 +0700

    fix file path

--- neurons/miners/file_utils.py
+++ neurons/miners/file_utils.py
@@ -125,7 +125,7 @@ def load_range_multi_process():
 if __name__ == '__main__':
     start_time = time.time_ns()
     file_path_template = "/home/ubuntu/c4-dataset/extracted/c4-train.0000{}-of-01024.json"
-    dir_path_template = "/home/ubuntu/c4-dataset/indexed_data/{}"
+    dir_path_template = "/home/ubuntu/c4-dataset/indexed_data/{}/"
     for i in range(1):
         FILE_PATH = file_path_template.format(str(i))
         DIR_PATH = dir_path_template.format("0000" + str(i))
