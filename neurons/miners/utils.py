4807442463c3a0a9c0be2c4bb6015a195ebb73eb
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-10 12:40:59 +0700

    convert file

--- neurons/miners/utils.py
+++ neurons/miners/utils.py
@@ -29,7 +29,21 @@ def create_one_column_file(source_path, dest_path):
                 count += 1
 
 
+def create_10000_file_from_merge_all(source_path, dest_dir_path):
+    db = 0
+    with open(source_path, 'r') as source_file:
+        for line in source_file:
+            data = line.strip().split(',')
+            converted_data = '\n'.join(data)
+            file_name = 'table_{}'.format(str(db))
+            file_path = dest_dir_path + '/' + file_name
+            with open(file_path, 'w') as des_file:
+                des_file.write(converted_data)
+            bt.logging.info("data at line {} converted".format(str(db)))
+            db += 1
+
+
 if __name__ == '__main__':
     arg1 = sys.argv[1]
     arg2 = sys.argv[2]
-    create_one_column_file(source_path=arg1, dest_path=arg2)
+    create_10000_file_from_merge_all(source_path=arg1, dest_path=arg2)
