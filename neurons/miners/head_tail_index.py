8314ff9f11621cdc639718fd1c9b63323539409d
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-13 23:01:08 +0700

    test head_tail_index

--- neurons/miners/head_tail_index.py
+++ neurons/miners/head_tail_index.py
@@ -71,3 +71,12 @@ def head_tail_api_pred_human_with_url(list_text, url):
         print('Failed to post data:status_code', response.status_code)
         print('Failed to post data:', response.content)
         return []
+
+if __name__ == '__main__':
+    urls =["http://69.67.150.21:8080/verify-data",
+           "http://103.219.170.221:8080/verify-data"]
+    list_text = ['As of now, there is no definitive answer to how many international tries James Hook has scored in his career as try scoring statistics are not always consistently recorded or easily accessible for all players. According to some sources, Hook has scored a total of 10 tries in 74 appearances for the Welsh national team between 2006 and 2017 (RugbyPass, 2021). However, this information may not be up-to-date or complete, as it is always possible that new tries have been scored since then.',
+                 'Formula:\n\n```\nLoss Percentage = (CP - SP) / CP *\n\n1. Calculate the loss amount: Loss = CP - SP\n   - CP = 605\n   - SP = 330\n   - Loss = 605 - 330 = 275\n\n2. Calculate the loss percentage:\n   - Loss Percentage = 275 / 605  100%\n```\n\n2. Ratio Method:\n\n- Calculate the ratio of SP to CP: 330 / 605 = 0.547\n- Multiply the ratio by 100 to express as a percentage: 0.547 * 100 = 54.7%\n- Prefix the result with a negative sign to inicate a loss: -54.7%\n\nNote:\n\n- Both alternative methods yield the same result as the original formula. - The percentage change formula is more concise and efficient for calculating losses. - The ratio method can be helpful in understanding the relationship between CP and SP visually.',
+                 'If your vehicle is involved in an accident, stolen or set. Explore Shropshire Council\u2019s new website, giving you the tools and information you need for the services we provide. Tom.co.uk helps dads find the right life insurance, for free. It\u2019s quick and easy, and means your family is protected financially if anything happened to you.']
+    result = head_tail_api_pred_human(list_text,urls)
+    print(result)
