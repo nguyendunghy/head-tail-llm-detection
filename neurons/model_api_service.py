26bc96368a72506dbb2b1fdce2b5dfe31c83b0cd
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-27 21:50:27 +0700

    update model controler change config

--- neurons/model_api_service.py
+++ neurons/model_api_service.py
@@ -5,6 +5,7 @@ import bittensor as bt
 from transformers.utils import logging as hf_logging
 
 from miners.gpt_zero import PPLModel
+from neurons import jackie_upgrade
 from neurons.app_config import AppConfig
 from neurons.miners.deberta_classifier import DebertaClassifier
 
@@ -30,6 +31,14 @@ class ModelService(ABC):
                                            device=device)
 
     def predict(self, input_data):
+        app_config = AppConfig()
+        bt.logging.info("app_config: " + str(app_config))
+        if app_config.allow_predict_50_50_standard_model():
+            return self.predict_50_50(input_data)
+        else:
+            return self.predict_baseline(input_data)
+
+    def predict_baseline(self, input_data):
         bt.logging.info("start predict")
         start_time = time.time()
         bt.logging.info(f"Amount of texts received: {len(input_data)}")
@@ -44,4 +53,21 @@ class ModelService(ABC):
         result = preds.tolist() if self.model_type != 'ppl' else preds
         bt.logging.info("predict_list: " + str(result))
         bt.logging.info(f"standard model predictions in {int(time.time() - start_time)}s")
+        return result
+
+    def predict_50_50(self, input_data):
+        bt.logging.info("start predict_50_50")
+        start_time = time.time()
+        bt.logging.info(f"Amount of texts received: {len(input_data)}")
+        try:
+            preds = self.model.predict_batch(input_data)
+        except Exception as e:
+            bt.logging.error('Could not proceed text "{}..."'.format(input_data))
+            bt.logging.error(e)
+            preds = [0] * len(input_data)
+
+        temp_pred = preds.tolist() if self.model_type != 'ppl' else preds
+        pred_list = jackie_upgrade.order_prob(temp_pred)
+        result = [1 if value else 0 for value in pred_list]
+        bt.logging.info(f"current_model_50_50_pred Made predictions in {int(time.time() - start_time)}s")
         return result
