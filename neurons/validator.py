8cd9c9ccbac079e54ba9fed9dfcb844cf68befe0
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-06-08 22:22:16 +0700

    add log

--- neurons/validator.py
+++ neurons/validator.py
@@ -77,7 +77,11 @@ class Validator(BaseValidatorNeuron):
         bt.logging.info(f"Generating texts for challenges...")
         data = self.generator.generate_data(n_human_samples=150, n_ai_samples=150)
         texts = [el for el in data]
+        bt.logging.info("===> TEXTS: " + str(texts))
+        bt.logging.info("===> TYPE TEXT: " + str(type(texts[0])))
         labels = np.array([int(el.label) for el in data])
+        bt.logging.info("===> LABELS: " + str(labels))
+        bt.logging.info("===> TYPE LABELS: " + str(type(labels[0])))
         return texts, labels
 
     async def forward(self):
